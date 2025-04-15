94f7309cfba1258eeaf417d69a720ea8446eda3b
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-15 19:54:07 +0530

    Button fixed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,15 @@
-// const Button = ({ name, isBeam = false, containerClass }) => {
-//     return (
-//       <button className={`btn ${containerClass}`}>
-//         {isBeam && (
-//           <span className="relative flex h-3 w-4">
-//             <span className="btn-ping" />
-//             <span className="btn-ping_dot" />
-//           </span>
-//         )}
-//         {name}
-//       </button>
-//     )
-//   }
+const Button = ({ name, isBeam = false, containerClass }) => {
+    return (
+      <button className={`btn ${containerClass}`}>
+        {isBeam && (
+          <span className="relative flex h-3 w-4">
+            <span className="btn-ping" />
+            <span className="btn-ping_dot" />
+          </span>
+        )}
+        {name}
+      </button>
+    )
+  }
   
-//   export default Button;
-
-const Button = ({ text, className, id }) => {
-  return (
-    <a onClick={(e)} => {
-      e.preventDefault();
-
-      const target = document.getElementById
-    }
-  )
-}
+  export default Button;
