76cf9f12677d14e91af57bdbd0c2dc9415124129
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-19 21:17:02 +0530

    Sections update

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -30,9 +30,6 @@ const Navbar = () => {
     <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
-          {/* <a href="/" className="text-neutral-400 font-bold text-ul hover:text-white transition-colors">
-            Akhil Shetty M
-          </a> */}
           <a
             href="/"
             className="hover:opacity-80 transition-opacity"
