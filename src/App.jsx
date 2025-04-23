657a7734a87424d885578d6e2ad25eb0282d5eb5
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-23 22:04:34 +0530

    imports of App jsx migrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import Navbar from "./sections/Navbar"
-import Hero from "./sections/Hero"
-import About from "./sections/About"
-import Projects from "./sections/Projects"
-import ProfessionalJourney from "./sections/ProfessionalJourney"
-import Contact from "./sections/Contact"
-import Footer from "./sections/Footer"
-import Experience from "./sections/Experience"
-import LogoSection from "./components/LogoSection"
-import FeatureCards from "./components/FeatureCards"
+import { Navbar, Hero, About, Projects, ProfessionalJourney, Contact, Footer, Experience, LogoSection, FeatureCards } 
+from "./imports"
 
 const App = () => {
   return (
---
+++ src/imports.js
@@ -0,0 +1,12 @@
+import Navbar from "./sections/Navbar"
+import Hero from "./sections/Hero"
+import About from "./sections/About"
+import Projects from "./sections/Projects"
+import ProfessionalJourney from "./sections/ProfessionalJourney"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+import Experience from "./sections/Experience"
+import LogoSection from "./components/LogoSection"
+import FeatureCards from "./components/FeatureCards"
+
+export { Navbar, Hero, About, Projects, ProfessionalJourney, Contact, Footer, Experience, LogoSection, FeatureCards }
