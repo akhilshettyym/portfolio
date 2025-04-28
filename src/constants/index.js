671fb9f953a1b4e9d435dc0faff73bca7cd8ac68
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-04-28 15:21:13 +0530

    Index cleaned and updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -171,9 +171,6 @@ export const abilities = [
 // Animated Words
 export const words = [
   { text: "PLANS", imgPath: "/images/animate/plans.svg" },
-  { text: "PLANS", imgPath: "/images/animate/star.png" },
-  { text: "PLANS", imgPath: "/images/animate/1.png" },
-  { text: "PLANS", imgPath: "/images/animate/2.png" },
   { text: "SPECS", imgPath: "/images/animate/3.png" },
   { text: "SAMPLES", imgPath: "/images/animate/samples.svg" },
   { text: "MODELS", imgPath: "/images/animate/models.svg" },
