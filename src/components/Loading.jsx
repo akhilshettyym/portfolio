75c0bbae3392bd1a46dc5118d3f098e566ea42d4
AKHIL SHETTY M <akhilshettym2003@gmail.com>
2025-05-08 22:59:00 +0530

    Loading update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -11,8 +11,7 @@ const CanvasLoader = () => {
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-      }}
-    >
+      }} >
       <span className="canvas-loader" />
       <p style={{ fontSize: 14, color: "#F1F1F1", fontWeight: 800, marginTop: 40 }}>
         {progress !== 0 ? `${progress.toFixed(2)}%` : "Loading..."}
